b569dfc974b29d46a8572dc91d785547f15d20cd
MephistoDevelop <inesherald@gmail.com>
2020-03-11 18:17:16 -0600

    Add migration from books

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_11_232542) do
+ActiveRecord::Schema.define(version: 2020_03_12_001616) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "category"
+    t.string "author"
+    t.string "read_percent"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
